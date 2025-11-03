d5755a8b6f3a846a481bcda1df8d7adad077cdc4
samuilivanov23 <samuil2001ivanov@gmail.com>
2025-11-03 20:43:56 +0200

    Adding exclamation mark to the zero_division test.

--- pytest_test/tests/test_my_functions.py
+++ pytest_test/tests/test_my_functions.py
@@ -24,10 +24,10 @@ def test_very_slow():
   result = my_functions.divide(9, 3)
   assert result == 3
 
-@pytest.mark.skip(reason="This feature is currently broken")
+@pytest.mark.skip(reason="This feature is currently broken!")
 def test_add():
   assert my_functions.add(1, 2) == 3
 
-@pytest.mark.xfail(reason="We know we cannot divide by zero")
+@pytest.mark.xfail(reason="We know we cannot divide by zero!")
 def test_divide_by_zero():
   my_functions.divide(4, 0)
