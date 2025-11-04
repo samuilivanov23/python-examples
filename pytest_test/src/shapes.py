81c06dc50e1b0bed896ae3c0cc5fb5c94820f54e
samuilivanov23 <samuil2001ivanov@gmail.com>
2025-11-04 12:46:06 +0200

    Adding cube shape and volume calculation function

--- pytest_test/src/shapes.py
+++ pytest_test/src/shapes.py
@@ -38,3 +38,10 @@ class Rectangle(Shape):
 class Square(Rectangle):
   def __init__(self, side_length):
     super().__init__(side_length, side_length)
+
+class Cube():
+  def __init__(self, length):
+    self.length = length
+  
+  def volume(self):
+    return self.length * self.length * self.length
