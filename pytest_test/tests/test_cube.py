aecfba2dff8cd022937b4829b9c44f0158cfe92e
samuilivanov23 <samuil2001ivanov@gmail.com>
2025-11-04 12:43:14 +0200

    Adding test for cube volume calculation

---
+++ pytest_test/tests/test_cube.py
@@ -0,0 +1,6 @@
+import pytest
+import src.shapes as shapes
+
+@pytest.mark.parametrize("side_length, expected_volume", [(3, 27), (5, 125), (4, 50)])
+def test_cube_volume(side_length, expected_volume):
+  assert shapes.Cube(side_length).volume() == expected_volume
